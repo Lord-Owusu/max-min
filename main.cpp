ff54990d1be94661ca1068d04d28e5f5046caf7a
Lord-Owusu <61398179+Lord-Owusu@users.noreply.github.com>
2020-02-24 02:45:47 +0000

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,51 @@
+ #include<iostream>
+
+    using namespace std;
+
+    int main ()
+
+    {
+
+        int arr[10], n, i, max, min;
+
+        cout << "Please Enter the size of the array : ";
+
+        cin >> n;
+
+        cout << "Please Enter the elements of the array : ";
+
+        for (i = 0; i < n; i++)
+
+            cin >> arr[i];
+
+        max = arr[0];
+
+        for (i = 0; i < n; i++)
+
+        {
+
+            if (max < arr[i])
+
+                max = arr[i];
+
+        }
+
+        min = arr[0];
+
+        for (i = 0; i < n; i++)
+
+        {
+
+            if (min > arr[i])
+
+                min = arr[i];
+
+        }
+
+        cout << "Largest element is : " << max;
+
+        cout << " and the sSmallest element is : " << min;
+
+        return 0;
+
+    }
